3c8d2feb7c91b76f246005151e1270e7473ca16b
alicesocialcde <temporaneo87@gmail.com>
2013-07-28 12:02:24 +0200

    Create HeatingSettings.java

---
+++ home_automation_app/src/com/acme/home_automation_app/HeatingSettings.java
@@ -0,0 +1,14 @@
+package com.acme.home_automation_app;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TabHost;
+
+public class HeatingSettings extends Activity {
+
+}
+  
