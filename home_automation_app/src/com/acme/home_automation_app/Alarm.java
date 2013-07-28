d377bcf250e22fb857b6db240f094f405f9cc1af
alicesocialcde <temporaneo87@gmail.com>
2013-07-28 12:07:35 +0200

    Create Alarm.java

---
+++ home_automation_app/src/com/acme/home_automation_app/Alarm.java
@@ -0,0 +1,13 @@
+package com.acme.home_automation_app;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TabHost;
+
+public class Alarm extends Activity {
+}
+  
